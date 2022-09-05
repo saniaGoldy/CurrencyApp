69d525cafbe4c029c7cc26c61a45e66183a8c45b
Sasha Riaboshapka <sashariaboshapka2003@gmail.com>
2022-09-05 15:39:16 +0300

    WebViewActivity.kt implemented

---
+++ app/src/main/java/com/example/currencyapp/ui/news/webPageFragment/WebViewActivity.kt
@@ -0,0 +1,150 @@
+package com.example.currencyapp.ui.news.webPageFragment
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.provider.Settings
+import android.view.KeyEvent
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.navArgs
+import com.example.currencyapp.R
+import com.example.currencyapp.databinding.ActivityWebViewBinding
+import com.example.currencyapp.domain.services.ConnectivityObserver
+import com.google.android.material.snackbar.Snackbar
+
+
+class WebViewActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWebViewBinding
+    private val viewModel: WebViewModel by viewModels()
+
+    private var progress: Progress? = null
+    private var isLoaded: Boolean = false
+    private var doubleBackToExitPressedOnce = false
+    //private var webURL = "https://www.geeklabs.co.in/" // Change it with your URL
+
+    private val args: WebViewActivityArgs by navArgs()
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityWebViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+
+            viewModel.networkStatus.observe(this@WebViewActivity) {
+                if (it == ConnectivityObserver.Status.Available) {
+                    showToast(getString(R.string.no_internet_connection_error_message))
+                    infoTV.text = getString(R.string.no_internet_connection_error_message)
+                    showNoNetSnackBar()
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        if (viewModel.networkStatus.value == ConnectivityObserver.Status.Available && !isLoaded) loadWebView()
+        super.onResume()
+    }
+
+    private fun loadWebView() = with(binding) {
+        infoTV.text = ""
+        webView.loadUrl(args.url)
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                val url = request?.url.toString()
+                view?.loadUrl(url)
+                return super.shouldOverrideUrlLoading(view, request)
+            }
+
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                setProgressDialogVisibility(true)
+                super.onPageStarted(view, url, favicon)
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                isLoaded = true
+                setProgressDialogVisibility(false)
+                super.onPageFinished(view, url)
+            }
+
+            override fun onReceivedError(
+                view: WebView,
+                request: WebResourceRequest,
+                error: WebResourceError
+            ) {
+                isLoaded = false
+                val errorMessage = "Got Error! $error"
+                showToast(errorMessage)
+                infoTV.text = errorMessage
+                setProgressDialogVisibility(false)
+                super.onReceivedError(view, request, error)
+            }
+        }
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean = with(binding) {
+        if (event.action == KeyEvent.ACTION_DOWN) {
+            if (keyCode == KeyEvent.KEYCODE_BACK) {
+                if (webView.canGoBack()) {
+                    webView.goBack()
+                } else {
+                    showToastToExit()
+                }
+                return true
+            }
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
+    private fun showToastToExit() {
+        when {
+            doubleBackToExitPressedOnce -> {
+                onBackPressed()
+            }
+            else -> {
+                doubleBackToExitPressedOnce = true
+                showToast(getString(R.string.back_again_to_exit))
+                Handler(Looper.myLooper()!!).postDelayed({
+                    doubleBackToExitPressedOnce = false
+                }, 2000)
+            }
+        }
+    }
+
+    private fun setProgressDialogVisibility(visible: Boolean) {
+        if (visible) progress = Progress(this, R.string.please_wait, cancelable = true)
+        progress?.apply { if (visible) show() else dismiss() }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showNoNetSnackBar() {
+        val snack =
+            Snackbar.make(
+                binding.root,
+                getString(R.string.no_internet_connection_error_message),
+                Snackbar.LENGTH_INDEFINITE
+            )
+        snack.setAction(getString(R.string.settings)) {
+            startActivity(Intent(Settings.ACTION_WIFI_SETTINGS))
+        }
+        snack.show()
+    }
+}
