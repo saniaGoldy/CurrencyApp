19eec6278af688adb620777aa5b1b4ada25d275c
Sasha Riaboshapka <sashariaboshapka2003@gmail.com>
2022-09-21 00:26:48 +0300

    API keys moved to build configs

--- app/src/main/java/com/example/currencyapp/data/remote/CurrencyAPI.kt
+++ app/src/main/java/com/example/currencyapp/data/remote/CurrencyAPI.kt
@@ -1,5 +1,6 @@
 package com.example.currencyapp.data.remote
 
+import com.example.currencyapp.BuildConfig
 import com.example.currencyapp.data.remote.entities.currencyFluctuation.CurrenciesFluctuationsResponse
 import com.example.currencyapp.data.remote.entities.currencyRateStory.CurrenciesRateStory
 import com.example.currencyapp.data.remote.entities.news.NewsResponse
@@ -10,11 +11,8 @@ import retrofit2.http.Query
 
 interface CurrencyAPI {
 
-    //key on main account
-    //@Headers("apikey: VN1OrfB9k7ytPwwPR5rQzJmgJhxGBAM0")
-
     //key on secondary account
-    @Headers("apikey: VN1OrfB9k7ytPwwPR5rQzJmgJhxGBAM0")
+    @Headers("apikey: ${BuildConfig.RATES_API_KEY}")
     @GET("/exchangerates_data/fluctuation")
     suspend fun getCurrencyFluctuation(
         @Query("start_date") startDate: String,
@@ -22,7 +20,7 @@ interface CurrencyAPI {
         @Query("base") base: String
     ): Response<CurrenciesFluctuationsResponse>
 
-    @Headers("apikey: VN1OrfB9k7ytPwwPR5rQzJmgJhxGBAM0")
+    @Headers("apikey: ${BuildConfig.RATES_API_KEY}")
     @GET("/exchangerates_data/timeseries")
     suspend fun getCurrencyRates(
         @Query("start_date") startDate: String,
@@ -38,7 +36,7 @@ interface CurrencyAPI {
 
     @param [keywords] Use this parameter to search for sentences, you can also exclude words that you do not want to appear in your search results. Example: To search for 'New bitcoin news 2021' but exclude 'Etherum': &sources=new bitcoin news 2021, -etherum
      */
-    @Headers("apikey: LZkmdbgS82SvAgwYDleTrwnYsabmoUEr")
+    @Headers("apikey: ${BuildConfig.NEWS_API_KEY}")
     @GET("/financelayer/news")
     suspend fun getCurrencyNews(
         @Query("tags") tags: String,
